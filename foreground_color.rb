cf439245f8078f5f6bdfa4f416f56ef43f41acd9
boykj <jboyk918@gmail.com>
2021-05-09 20:04:47 -0700

    foreground_color.rb: Add attr_reader for paint vars

--- foreground_color.rb
+++ foreground_color.rb
@@ -1,7 +1,13 @@
 class ForegroundColor
 
+	attr_reader :label_text, :x, :y, :foreground_color
+
 	def darken_foreground
 		paint(label_text, x, y, foreground_color - 10, '#111111')
 	end
-	
+
+	def lighten_foreground
+		paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+	end
+
 end
