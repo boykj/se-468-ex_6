eee714ba3e0e04765b94868d3f36e1453016defd
boykj <jboyk918@gmail.com>
2021-05-09 21:35:26 -0700

    main.rb: Add object creation for ForegroundColor class

--- main.rb
+++ main.rb
@@ -3,14 +3,15 @@ require_relative 'foreground_color'
 class Button
 	
 	attr_reader :is_dark_mode
+	color = ForegroundColor.new()
 
 	def draw_button
     if is_dark_mode
       # darken foreground color for dark mode
-      darken_foreground
+      color.darken_foreground
     else
       # lighten foreground color for non-dark mode
-      lighten_foreground
+      color.lighten_foreground
     end
   end
 
