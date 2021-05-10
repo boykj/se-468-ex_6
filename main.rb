5c0065874867dcf21c360458d72e8e0223f20f5e
boykj <jboyk918@gmail.com>
2021-05-09 19:54:09 -0700

    main.rb: Add Button class

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+class Button
+
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
     if is_dark_mode
       # darken foreground color for dark mode
@@ -7,3 +9,5 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
       paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
     end
   end
+
+end
